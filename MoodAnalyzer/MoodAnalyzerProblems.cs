41f587af2a67c6c56e40043204290cc3048bace8
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-10-11 23:10:18 +0530

    [Pratu]ADDED: ability to analyse and respond Happy or Sad Mood

---
+++ MoodAnalyzer/MoodAnalyzerProblems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+   public class MoodAnalyzerProblems
+    {
+        public string AnalyseMood(string moodMessage)
+        {
+            if (moodMessage.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
+    
+
